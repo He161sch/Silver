16889f4a52dbb4190adfeefdf6eb4fde5d90a88e
Heiko Schmid <heikoschmid10@gmail.com>
2020-11-13 14:03:42 +0100

    MVC architecture

--- src/main/scala/aview/TUI.scala
+++ src/main/scala/aview/TUI.scala
@@ -2,53 +2,33 @@ package aview
 
 
 import controller.Controller
-import model.{Card, Hand, Player}
-import util.{Observer, Observable}
+import util.Observer
 
 class TUI(controller: Controller) extends Observer{
 
   controller.add(this)
 
-  def processInputLine(input: String, player: Player, newCard: Card): Player = {
+  def processInputLine(input: String): Unit = {
     val inputsplit = input.split(" ").toList
     inputsplit match{
       case _ =>
-        if (inputsplit.head.matches("d") && inputsplit(1).matches("1|2|3|4|0")){
-          printf("card value: %d \n", player.hand.cards(inputsplit(1).toInt).number)
+        if (inputsplit.head.matches("d")){
+          controller.drawCard()
+        } else if (inputsplit.head.matches("v") && inputsplit(1).matches("1|2|3|4|0")){
+          controller.viewCard(inputsplit(1).toInt)
         } else if (inputsplit.head.matches("s") && inputsplit(1).matches("1|2|3|4|0")){
-          val newPlayer = controller.switchCard(player, inputsplit(1).toInt, newCard)
-          return newPlayer
+          controller.switchCard(inputsplit(1).toInt)
         } else if (inputsplit.head.matches("e")){  //end
-          printf("your points: %d", player.hand.handValue())
+          controller.showHandValue()
           System.exit(0)
         } else if (inputsplit.head.matches("c") && inputsplit(1).matches("1|2|3|4|0") && inputsplit(2).matches("1|2|3|4|0")) {   //combine
-          val newPlayer = controller.combineCard(player, inputsplit(1).toInt, inputsplit(2).toInt, newCard)
-          return newPlayer
+          controller.combineCard(inputsplit(1).toInt, inputsplit(2).toInt)
+        }else{
+          println("ungültiger befehl")
         }
-        player
+
     }
   }
-//  def removeAtIdx[T](idx: Int, listToRemoveFrom: List[T]): List[T] = {
-//    assert(listToRemoveFrom.length > idx && idx >= 0)
-//    val (left, _ :: right) = listToRemoveFrom.splitAt(idx)
-//    left ++ right
-//  }
 
-//  def switchCard(player:Player, idx: Int, newCard: Card): Player = {
-//    val newPlayer = Player(player.name, Hand(player.hand.cards.patch(idx, List(newCard), 1)))
-//    newPlayer
-//  }
-//
-//  def combineCard(player: Player, idx1: Int, idx2: Int, newCard: Card): Player ={
-//    if(player.hand.cards(idx1).number.equals(player.hand.cards(idx2).number)){
-//      val hand = Hand(player.hand.cards.updated(idx1, newCard))
-//      val newPlayer = Player(player.name, Hand(player.hand.removeAtIdx(idx2, hand.cards)))
-//      newPlayer
-//    } else {
-//      printf("card values are not the same! (%d, %d)\n",player.hand.cards(idx1).number, player.hand.cards(idx2).number)
-//      player
-//    }
-//  }
-
-  override def update: Unit = println(controller.playerToString)
+  override def update: Unit = controller.playerToString
 }
--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -1,28 +1,80 @@
 package controller
 
-import model.{Hand, Card, Player}
+import model.{Card, Hand, Player}
 import util.Observable
 
-class Controller() extends Observable {
+import scala.util.Random
+import scala.util.control.Breaks.break
 
-  def switchCard(player:Player, idx: Int, newCard: Card): Player = {
-    val newPlayer = Player(player.name, Hand(player.hand.cards.patch(idx, List(newCard), 1)))
-    notifyObservers
-    newPlayer
+
+class Controller() extends Observable {
+  val r: Random.type = scala.util.Random
+  var p1: Player = createPlayer()
+  var newCard: Card = Card(0)
+
+
+  def createPlayer(): Player ={
+    Player("Player 1", randomHand())
   }
 
-  def combineCard(player: Player, idx1: Int, idx2: Int, newCard: Card): Player ={
-    if(player.hand.cards(idx1).number.equals(player.hand.cards(idx2).number)){
-      val hand = Hand(player.hand.cards.updated(idx1, newCard))
-      val newPlayer = Player(player.name, Hand(player.hand.removeAtIdx(idx2, hand.cards)))
+
+  def viewCard(idx: Int): Unit ={
+    printf("Card Value: %d\n", p1.hand.cards(idx).number)
+    println(p1.toString)
+  }
+
+
+  def switchCard(idx: Int): Unit = {
+    p1 = Player(p1.name, Hand(p1.hand.cards.patch(idx, List(newCard), 1)))
+    println(p1.toString)
+    notifyObservers
+  }
+
+
+  def showHandValue(): Unit ={
+    println(p1.toString)
+    println(p1.hand.handValue())
+  }
+
+
+  def combineCard(idx1: Int, idx2: Int): Unit ={
+    if (idx1 >= p1.hand.cards.size || idx2 >= p1.hand.cards.size){
+      println("check number of cards")
+      println(p1.toString)
+      return
+    }
+    if(p1.hand.cards(idx1).number.equals(p1.hand.cards(idx2).number)){
+      val hand = Hand(p1.hand.cards.updated(idx1, newCard))
+      p1 = Player(p1.name, Hand(p1.hand.removeAtIdx(idx2, hand.cards)))
+      println(p1.toString)
       notifyObservers
-      newPlayer
     } else {
-      printf("card values are not the same! (%d, %d)\n",player.hand.cards(idx1).number, player.hand.cards(idx2).number)
+      printf("card values are not the same! (%d, %d)\n",p1.hand.cards(idx1).number, p1.hand.cards(idx2).number)
+      println(p1.toString)
       notifyObservers
-      player
     }
   }
 
+  def drawCard(): Unit = {
+    newCard = Card(r.nextInt(14))
+    printf("new Card = %d\n", newCard.number)
+    println(p1.toString)
+  }
+
+
+  def handToString: String = Hand.toString()
+
+
   def playerToString: String = Player.toString()
+
+
+  def randomHand(): Hand ={
+    val r = scala.util.Random
+    var cards = List[Card]()
+    for(x <- 0 to 4){
+      cards = Card(r.nextInt(14)) :: cards
+    }
+    val hand = Hand(cards)
+    hand
+  }
 }
