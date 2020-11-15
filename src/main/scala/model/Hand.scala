bf6295ba9bd4d00c0c6fb04918505022fc578623
MarcoMollo <marcomollo@gmx.de>
2020-11-15 10:31:55 +0100

    TUI

--- src/main/scala/model/Hand.scala
+++ src/main/scala/model/Hand.scala
@@ -3,5 +3,17 @@ package model
 
 case class Hand(cards: List[Card]){
   override def toString: String = "[" + cards.mkString(", ") + "]\n"
-  def handValue(): Int = cards.foldLeft(0)(_ + _) //Verbessern fehler finden!
+  //def handValue(): Int = cards.foldLeft(0)(_ + _) //Verbessern fehler finden!
+  def handValue(): Int ={
+    var sum: Int = 0
+    for (card <- cards){
+      sum += card.number
+    }
+    sum
+  }
+  def removeAtIdx[T](idx: Int, listToRemoveFrom: List[T]): List[T] = {
+    assert(listToRemoveFrom.length > idx && idx >= 0)
+    val (left, _ :: right) = listToRemoveFrom.splitAt(idx)
+    left ++ right
+  }
 }
