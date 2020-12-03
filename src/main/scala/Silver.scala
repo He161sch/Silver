fbd198f218a3bc5bfc4739dc1bb56f69f2ef6c42
Heiko Schmid <heikoschmid10@gmail.com>
2020-12-03 12:04:20 +0100

    pattern

--- src/main/scala/Silver.scala
+++ src/main/scala/Silver.scala
@@ -4,18 +4,26 @@ import aview.TUI
 import controller.Controller
 
 
-object Silver {
+object Silver extends Runnable {
 
   val controller = new Controller()
   val tui = new TUI(controller)
   controller.notifyObservers
 
-  def main(args: Array[String]): Unit = {
-    var input: String = args(0)
-    if (!input.isEmpty) tui.processInputLine(input)
-    else do{
+  def run(): Unit = {
+    var input: String = " "
+    do{
       input = readLine()
       tui.processInputLine(input)
     } while (input != "q")
+  }
+
+  def main(args: Array[String]): Unit = {
+//    var input: String = " "
+//    do{
+//      input = readLine()
+//      tui.processInputLine(input)
+//    } while (input != "q")
+    Silver.run()
   }
 }
