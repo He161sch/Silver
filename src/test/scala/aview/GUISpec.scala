3b4e4badf162d00a3686063c43034092a49696d5
MarcoMollo <marcomollo@gmx.de>
2020-12-03 22:47:39 +0100

    Patterns Tests

--- src/test/scala/aview/GUISpec.scala
+++ src/test/scala/aview/GUISpec.scala
@@ -1,5 +1,7 @@
 package aview
 
+import java.io.ByteArrayOutputStream
+
 import controller.Controller
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -9,7 +11,20 @@ class GUISpec extends AnyWordSpec with Matchers{
     val controller = new Controller()
     val gui = new GUI(controller)
     "should have this String representation when updated" in {
+      val output = new ByteArrayOutputStream()
+      Console.withOut(output){
+        controller.ObserverInState()
+      }
+      output.toString should include ("Try it with TUI again")
+    }
 
+    "should have this output when command is processed" in {
+      gui.inputCommand("some input")
+      val output = new ByteArrayOutputStream()
+      Console.withOut(output){
+        controller.ObserverInState()
+      }
+      output.toString should include ("Try it with TUI again")
     }
 
   }
