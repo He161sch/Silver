079185f13af2a56a068818265cf0b8538d061af2
MarcoMollo <marcomollo@gmx.de>
2021-01-23 12:58:47 +0100

    small updates

--- src/main/scala/de/htwg/se/Silver.scala
+++ src/main/scala/de/htwg/se/Silver.scala
@@ -1,19 +1,25 @@
 package de.htwg.se
 
-import com.google.inject.Guice
-import de.htwg.se.aview.TUI
+import com.google.inject.{Guice, Injector}
+import de.htwg.se.aview.{TUI, UIFactory}
 import de.htwg.se.aview.gui.WelcomePanel
 import de.htwg.se.controller.controllercomponent.ControllerInterface
 
-object Silver {
-  val injector = Guice.createInjector(new SilverModule)
-  val controller = injector.getInstance(classOf[ControllerInterface])
-  val tui = new TUI(controller)
+import scala.util.{Failure, Success, Try}
 
+object Silver {
+  val injector: Injector = Guice.createInjector(new SilverModule)
+  val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
+
+
+  val uiType: String = if (System.getenv("UI_TYPE") == null) "both" else System.getenv("UI_TYPE")
 
   def main(args: Array[String]): Unit = {
-    val gui = new WelcomePanel(controller)
-    gui.visible = true
-    tui.run()
+
+    Try(UIFactory(uiType, controller)) match {
+      case Failure(v) => println("Could not start UI because: " + v.getMessage + v.printStackTrace())
+      case Success(v) => println("GOOD BYE")
+    }
   }
 }
+
--- src/main/scala/de/htwg/se/aview/UIFactory.scala
+++ src/main/scala/de/htwg/se/aview/UIFactory.scala
@@ -1,15 +1,20 @@
-//package de.htwg.se.aview
-//
-//import de.htwg.se.aview.gui.GUI
-//import controller.controllerComponent.Controller
-//
-//trait UIFactory {
-//  def processCommands(input: String)
-//}
-//
-//object UIFactory {
-//  def apply(kind: String, controller: Controller) = kind match {
-//    case "GUI" => new GUI(controller).run()
-//    case "TUI" => new TUI(controller).run()
-//  }
-//}
+package de.htwg.se.aview
+
+import de.htwg.se.aview.gui.WelcomePanel
+import de.htwg.se.controller.controllercomponent.ControllerInterface
+
+
+trait UIFactory {
+  def processCommands(input: String)
+}
+
+object UIFactory {
+  def apply(kind: String, controller: ControllerInterface) = kind match {
+    case "GUI" | "gui" => new WelcomePanel(controller)
+    case "TUI" | "tui" => new TUI(controller).run()
+    case "both" | "BOTH" => {
+      new WelcomePanel(controller)
+      new TUI(controller).run()
+    }
+  }
+}
